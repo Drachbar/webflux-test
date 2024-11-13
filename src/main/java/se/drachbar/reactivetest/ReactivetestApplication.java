3bc2f9fac066565ed13f1e52afe63888273a6487
Mattias <drachbar@gmail.com>
2024-11-14 00:33:59 +0100

    init

---
+++ src/main/java/se/drachbar/reactivetest/ReactivetestApplication.java
@@ -0,0 +1,15 @@
+package se.drachbar.reactivetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivetestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivetestApplication.class, args);
+
+
+    }
+
+}
